344896c0c1357c88bd70fe8e9dc66c197778e8d9
shivanshu <shivshrit2@gmail.com>
2018-07-16 19:46:38 +0530

    double array

--- doublearray_maps.rb
+++ doublearray_maps.rb
@@ -1,20 +1,21 @@
+# your comment
 class Double
   def double_array(arr)
-   arr.map{|n| n*2}
+    arr.map { |n| n * 2 }
   end
 end
 
 input_array = []
 ob = Double.new
 
-puts "Enter length of array"
+puts 'Enter length of array'
 arr_length = gets.chomp.to_i
-while arr_length <= 0 do
- puts "negative or zero length not allowed"
- arr_length = gets.chomp.to_i
+while arr_length <= 0
+  puts 'negative or zero length not allowed'
+  arr_length = gets.chomp.to_i
 end
 
-puts "Enter array values for array"
+puts 'Enter array values for array'
 
 arr_length.times do |n|
   input_array[n] = gets.chomp.to_i
@@ -22,6 +23,3 @@ end
 
 double_arr = ob.double_array(input_array)
 print double_arr
-
-
-
