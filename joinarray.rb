1467ae62fe024db65bcf29ba930cb0843932f4a1
shivanshu <shivshrit2@gmail.com>
2018-07-16 19:56:02 +0530

    joining array

--- joinarray.rb
+++ joinarray.rb
@@ -1,38 +1,36 @@
+# your comment
 class Array
-  def join(first_arr,second_arr)
-	  first_arr.concat(second_arr)
+  def join(first_arr, second_arr)
+    first_arr.concat(second_arr)
   end
 end
 
-
-object = Array.new
+object = []
 
 first_arr = []
-second_arr= []
+second_arr = []
 
-puts "Enter length of first array"
+puts 'Enter length of first array'
 first_arr_length = gets.chomp.to_i
-while first_arr_length <= 0 do
- puts "negative or zero length not allowed"
+while first_arr_length <= 0
+ puts 'negative or zero length not allowed'
  first_arr_length = gets.chomp.to_i
 end
-puts "Enter array values for first array"
+puts 'Enter array values for first array'
 first_arr_length.times do |n|
   first_arr[n] = gets.chomp.to_i
 end
 
-puts "Enter length of second array"
+puts 'Enter length of second array'
 second_arr_length = gets.chomp.to_i
-while second_arr_length <= 0 do
- puts "negative or zero length not allowed"
+while second_arr_length <= 0
+ puts 'negative or zero length not allowed'
  second_arr_length = gets.chomp.to_i
 end
-puts "Enter array values for second"
+puts 'Enter array values for second'
 second_arr_length.times do |n|
   second_arr[n] = gets.chomp.to_i
 end
 
-output_arr = object.join(first_arr,second_arr)
+output_arr = object.join(first_arr, second_arr)
 print output_arr
-
-
