0d2fa424b3468eb14903c68370d464e6fa899468
shivanshu <shivshrit2@gmail.com>
2018-07-16 20:23:56 +0530

    printing star

--- starpyramid.rb
+++ starpyramid.rb
@@ -1,23 +1,20 @@
+# your comment
 class Pyramid
   def print_star(space)
     (0...space).each do |i|
       (0..space).each do
-        printf " "
+        print ' '
       end
-     (0...2*i+1).each do
-       printf "*"
+     (0...2 * i + 1).each do
+       printf '*'
      end
-     puts "\n"
-     space -=1
+      puts "\n"
+      space -= 1
     end
   end
 end
 
-
-puts "Input size of pyramid to be printed"
+puts 'Input size of pyramid to be printed'
 size = gets.chomp.to_i
 p = Pyramid.new
 p.print_star(size)
-
-
-
