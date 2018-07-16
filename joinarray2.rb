e10f5d508b37c63d26db1b3c968d7f8f07003ab5
shivanshu <shivshrit2@gmail.com>
2018-07-16 20:00:05 +0530

    join array

--- joinarray2.rb
+++ joinarray2.rb
@@ -1,40 +1,38 @@
-class Joining_array
-  def join_array(first_arr,second_arr)
-    second_arr.each{|val| first_arr.push(val)}
-   return first_arr
-   end
+# your comment
+class JoiningArray
+  def join_array(first_arr, second_arr)
+    second_arr.each { |val| first_arr.push(val) }
+    first_arr
+  end
 end
-
 
 first_arr = []
 second_arr = []
-object = Joining_array.new  
+object = Joining_array.new
 
-puts "Enter length of first array"
+puts 'Enter length of first array'
 first_arr_length = gets.chomp.to_i
-while first_arr_length <= 0 do
- puts "negative or zero length not allowed"
+while first_arr_length <= 0
+ puts 'negative or zero length not allowed'
  first_arr_length = gets.chomp.to_i
 end
 
-puts "Enter array values for first array"
+puts 'Enter array values for first array'
 first_arr_length.times do |n|
   first_arr[n] = gets.chomp.to_i
 end
 
-puts "Enter length of second array"
+puts 'Enter length of second array'
 second_arr_length = gets.chomp.to_i
-while second_arr_length <= 0 do
- puts "negative or zero length not allowed"
+while second_arr_length <= 0 
+ puts 'negative or zero length not allowed'
  second_arr_length = gets.chomp.to_i
 end
 
-
-puts "Enter array values for second"
+puts 'Enter array values for second'
 second_arr_length.times do |n|
   second_arr[n] = gets.chomp.to_i
 end
 
-concat_arr = object.join_array(first_arr,second_arr)
+concat_arr = object.join_array(first_arr, second_arr)
 puts concat_arr
-
