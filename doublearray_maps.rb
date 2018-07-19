b67b9b816cc9c1b26eb730d9162c59e2f29fcfea
shivanshu <shivshrit2@gmail.com>
2018-07-19 19:05:57 +0530

    joining array using maps

--- doublearray_maps.rb
+++ doublearray_maps.rb
@@ -1,25 +1,31 @@
 # your comment
-class Double
-  def double_array(arr)
-    arr.map { |n| n * 2 }
+class DoublearrayMaps
+  def doubling_array(input_array)
+    input_array.map { |n| n.to_i * 2 }
   end
 end
 
 input_array = []
-ob = Double.new
+
+LENGTH = /\A\d+\z/
+ARRAY_VALUES = /\A[+-]?\d+(\.[\d]+)?\z/
 
 puts 'Enter length of array'
-arr_length = gets.chomp.to_i
-while arr_length <= 0
-  puts 'negative or zero length not allowed'
-  arr_length = gets.chomp.to_i
+array_length = gets.chomp
+while LENGTH.match(array_length).nil? == true
+  puts 'Invalid input!Please enter a valid input'
+  array_length = gets.chomp
 end
 
 puts 'Enter array values for array'
-
-arr_length.times do |n|
-  input_array[n] = gets.chomp.to_i
+array_length.to_i.times do |n|
+  input_array[n] = gets.chomp
+  while ARRAY_VALUES.match(input_array[n]).nil? == true
+    puts 'Invalid input!Please enter a valid input'
+    input_array[n] = gets.chomp
+  end
 end
 
-double_arr = ob.double_array(input_array)
+double_object = DoublearrayMaps.new
+double_arr = double_object.doubling_array(input_array)
 print double_arr
