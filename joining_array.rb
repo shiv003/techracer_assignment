af2eacf3eec1b2b5ec28688c03e33538af26426a
shivanshu <shivshrit2@gmail.com>
2018-07-28 14:08:00 +0530

    Join : Join the two arrays without using builtin function

--- joining_array.rb
+++ joining_array.rb
@@ -1,46 +1,46 @@
 # your comment
 class JoiningArray
-  def join_array(first_arr, second_arr)
-    second_arr.each { |val| first_arr.push(val) }
-    first_arr
+  def join_array(first_input, second_input)
+    second_input.each { |val| first_input.push(val) }
+    first_input
   end
 end
 LENGTH = /\A\d+\z/
 ARRAY_VALUES = /\A[+-]?\d+(\.[\d]+)?\z/
 
-first_array = []
+first_input = []
 puts 'Enter length of first array'
-firstarray_length = gets.chomp
-while LENGTH.match(firstarray_length).nil? == true
+first_input_length = gets.chomp
+while LENGTH.match(first_input_length).nil? 
   puts 'Invalid input!Please enter a valid input'
-  firstarray_length = gets.chomp
+  first_input_length = gets.chomp
 end
 
 puts 'Enter array values for first array'
-firstarray_length.to_i.times do |n|
-  first_array[n] = gets.chomp
-  while ARRAY_VALUES.match(first_array[n]).nil? == true
+first_input_length.to_i.times do |n|
+  first_input[n] = gets.chomp
+  while ARRAY_VALUES.match(first_input[n]).nil? 
     puts 'Invalid input!Please enter a valid input'
-    first_array[n] = gets.chomp
+    first_input[n] = gets.chomp
   end
 end
 
-second_array = []
+second_input = []
 puts 'Enter length of second array'
-secondarray_length = gets.chomp
-while LENGTH.match(secondarray_length).nil? == true
+second_input_length = gets.chomp
+while LENGTH.match(second_input_length).nil? 
   puts 'Invalid input!Please enter a valid input'
-  secondarray_length = gets.chomp
+  second_input_length = gets.chomp
 end
 
 puts 'Enter array values for second array'
-secondarray_length.to_i.times do |n|
-  second_array[n] = gets.chomp
-  while ARRAY_VALUES.match(second_array[n]).nil? == true
+second_input_length.to_i.times do |n|
+  second_input[n] = gets.chomp
+  while ARRAY_VALUES.match(second_input[n]).nil? 
     puts 'Invalid input!Please enter a valid input'
-    second_array[n] = gets.chomp
+    second_input[n] = gets.chomp
   end
 end
-joinarray_object = JoiningArray.new
-concat_arr = joinarray_object.join_array(first_array, second_array)
-puts concat_arr
+join_array = JoiningArray.new
+concat_output = join_array.join_array(first_input, second_input)
+puts concat_output
